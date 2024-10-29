e64e1f9571f848bbb1a69ce308ba5b7c0981a089
mo <mo.itmo25@gmail.com>
2024-10-29 19:04:53 +0300

    hw_2

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,3 +1,18 @@
+from lecture_2.hw.shop_api.controllers import cart, item
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI(title="Shop API")
+
+@app.get(
+    path="/ping"
+)
+async def pong():
+    return {"message": "pong"}
+
+app.include_router(cart.router)
+app.include_router(item.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
