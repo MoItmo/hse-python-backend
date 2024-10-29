ea1059c13a9a926d72333d86b67caddc0fca9470
mo <mo.itmo25@gmail.com>
2024-10-29 19:05:48 +0300

    hw_3

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,6 +1,8 @@
 from lecture_2.hw.shop_api.controllers import cart, item
 import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from http import HTTPStatus
 
 app = FastAPI(title="Shop API")
 
@@ -10,9 +12,17 @@ app = FastAPI(title="Shop API")
 async def pong():
     return {"message": "pong"}
 
+@app.get(
+    path="/invalid",
+    status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+)
+async def invalid():
+    return {"message": "Invalid request"}
+
 app.include_router(cart.router)
 app.include_router(item.router)
 
+Instrumentator().instrument(app).expose(app)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
